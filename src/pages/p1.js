254628321580148eb7e2307d7832e8d315ace3bf
Arham Hayat <ar.hayat7@gmail.com>
2022-04-06 02:26:55 +0530

    Add files via upload

--- src/pages/p1.js
+++ src/pages/p1.js
@@ -11,42 +11,70 @@ const Page = () => {
 				<button class="bg-white h-10 w-24 rounded-r">Search</button></span>
 			</div>
 		</div>
-		<div class="cntr mx-8 w-9/10 block md:mx-40 mt-6 md:w-7/10 md:inline-flex" style={{ justifyContent:'space-around'}}>
-			<div class='left w-full md:w-6/9' > 
-			<Card style={{ height: '350px'}}>
-        <Card.Img variant="top" src="holder.js/100px160" />
+		<div class="cntr mx-8 w-9/10 block md:mx-40 mt-6 md:inline-flex " style={{ justifyContent:'space-around'}}>
+			<div class='left w-full md:w-2/3' > 
+			<Card style={{ height: '350px' , marginBottom:'5px'}}>
         <Card.Body>
-          <Card.Title>Job Description</Card.Title>
+          <Card.Title class="text-3xl">Hardware Technician</Card.Title>
+          <br/>
+          <br/>
+          <div class="inline-flex w-full">
+          <img class='h-24 w-24 ml-12'/>
+          <div class="txt">
+          <p>Tags</p>
+          <p>ratings</p>
+          <button class="bg-white text-black py-2 px-4  rounded">
+  		Apply Now</button>
+  		</div>
+  		</div>
+        </Card.Body>
+      </Card>
+      <Card style={{ height: '300px', marginBottom:'5px'}}>
+        <Card.Body>
+          <Card.Title class='text-left text-3xl'>Job Description</Card.Title>
+          <br/>
           <Card.Text>
             This is a longer card with supporting text below as a natural
             lead-in to additional content. This content is a little bit longer.
           </Card.Text>
         </Card.Body>
       </Card>
-      <Card style={{ height: '450px'}}>
-        <Card.Body>
-          <Card.Title>Job Description</Card.Title>
-          <Card.Text>
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      </div>
-      <div class="w-full md:w-3/9">
-      <Card style={{ height: '400px'}}>
+      <Card style={{height: '300px'}}>
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
+    <Card.Title class="text-left text-2xl text-black">Job Details</Card.Title>
+    <br/>
+    <p>Xyz</p>
+  </Card.Body>
+</Card>
+<Card style={{height: '100px'}}>
+  <Card.Body>
+    <br/>
+    <span><p class='float-left'>Job ID</p><p class='float-right'>#123456</p></span>
+  </Card.Body>
+</Card>
+      </div>
+      <div class="w-full md:w-96">
+      <Card style={{ backgroundColor:'black', height: '450px'}}>
+  <Card.Body>
+    <Card.Title class="text-left text-2xl text-white">Let Us Call You</Card.Title>
+    <br/>
+    <p class="text-left text-white label-input100">Name</p>
+	<input class="input100 mb-0 h-8 w-full" type="text" name="name" placeholder="Name"/>
+	<br/>
+	<p class="text-left text-white label-input100">Email</p>
+	<input class="input100 mb-0 h-8 w-full" type="text" name="email" placeholder="Email"/>
+	<br/>
+	<p class="text-left text-white label-input100">Message</p>
+	<textarea class="input100 mb-0 w-full" type="text" rows='5' cols='60' name="msg" placeholder="Message"/>
+	<br/>
+	<button class="bg-white text-black py-2 px-4 float-left rounded">
+  		Button
+	</button>
+
   </Card.Body>
 </Card>
 </div>
+
 		</div>
 		</div>
 
