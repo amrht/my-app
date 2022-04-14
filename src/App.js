6617210179b680ab97435a0119ed805bb1e2c718
Inzemam ul-Haq <Inzemamhaq05@gmail.com>
2022-04-14 06:23:50 +0000

    some changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navigation from './components/navbar.js'
 import Footer from './components/footer.js'
 import { CDBFooter } from 'cdbreact';
 import Cardpage from './pages/p2.js';
-import Page4 from './pages/p4.js';
+import Page from './pages/p4.js';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
@@ -15,8 +15,8 @@ function App() {
     <div className="App">
       <Navigation sticky='top' />
       <Switch>
-      	<Route path='/' component={Cardpage}/>
-      	<Route path='/thisjob' component={Page4}/>
+      	<Route path='/' component={Page}/>
+      	<Route path='/thisjob' component={Page}/>
       </Switch>
     
       
