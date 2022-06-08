e78056c907f78662c4f139928e797713c4295b4f
Arham Hayat <ar.hayat7@gmail.com>
2022-06-08 17:59:59 +0530

    Add files via upload

---
+++ src/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/navbar.js'
+import Footer from './components/footer.js'
+import { CDBFooter } from 'cdbreact';
+import Cardpage from './pages/p2.js';
+import Page from './pages/p4.js';
+import Page3 from './pages/p3.js';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+     
+    <div className="App">
+      <Navigation sticky='top' />
+      <Switch>
+      	<Route path='/' component={Page}/>
+      	<Route path='/thisjob' component={Cardpage}/>
+      </Switch>
+    
+      
+
+
+    </div>
+     
+  );
+}
+
+export default App;
